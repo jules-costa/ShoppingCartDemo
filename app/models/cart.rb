961abc2cf5b91493434d45654f6e426ede3cc855
Julianne Costa <julesleighcosta@gmail.com>
2017-08-02 12:15:18 -0700

    add and remove products successfully

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -4,4 +4,62 @@ class Cart < ApplicationRecord
   has_many :products,
     through: :selections,
     source: :product
+
+  def self.checkout
+    in_stock = []
+    sold_out = []
+    items = active_cart.products
+    items.each do |item|
+      if item.available_inventory > 0
+        in_stock << item
+        item.available_inventory -= 1
+      else
+        sold_out << item
+      end
+    end
+    active_cart.active = false
+    active_cart.save!
+    display_message(sold_out)
+    generate_receipt(in_stock)
+  end
+
+  def self.view_history
+    history = Cart.where(active: false).to_a
+  end
+
+  def add_product(title)
+    product = Product.where(title: title).to_a
+    Selection.create!(product_id: product.id, cart_id: active_cart.id)
+  end
+
+  def remove_product(title)
+    item = Product.where(title: title).to_a
+    record = Selection.where(cart_id: Cart.active_cart.id, product_id: item.id).limit(1).to_a
+    Selection.destroy(record.id)
+  end
+
+  def view_cart
+    active_cart.products
+  end
+
+  private
+
+  def self.inactive_carts
+    Cart.where(active: false).to_a
+  end
+
+  def self.active_cart
+    Cart.where(active: true).to_a
+  end
+
+  def display_message(array)
+    x = array.map { |el| el.title }.join(", ")
+    puts "We're sorry, but we're sold out of #{x}."
+  end
+
+  def generate_receipt(array)
+    x = array.map { |el| el.title }
+    puts "Thank you for your order of: #{x}"
+  end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Product.destroy_all
+Cart.destroy_all
+Selection.destroy_all
+
+product1 = Product.create!(price: 100.00, title: "jumpsuit", available_inventory: 2)
+product2 = Product.create!(price: 70.00, title: "slacks", available_inventory: 3)
+product3 = Product.create!(price: 30.50, title: "tank top", available_inventory: 4)
+product4 = Product.create!(price: 120.50, title: "jeans", available_inventory: 5)
+product5 = Product.create!(price: 50.25, title: "blouse", available_inventory: 1)
+product6 = Product.create!(price: 70.50, title: "skirt", available_inventory: 3)
+product7 = Product.create!(price: 200.00, title: "sneakers", available_inventory: 1)
+product8 = Product.create!(price: 100.00, title: "backpack", available_inventory: 7)
+product9 = Product.create!(price: 60.00, title: "swimsuit", available_inventory: 2)
+product10 = Product.create!(price: 75.75, title: "watch", available_inventory: 3)
+product11 = Product.create!(price: 35.00, title: "earrings", available_inventory: 4)
+product12 = Product.create!(price: 350.50, title: "dress", available_inventory: 5)
+
+cart1 = Cart.create!(active: false)
+cart2 = Cart.create!
