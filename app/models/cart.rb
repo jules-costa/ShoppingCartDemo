bb23136d0c849d825d25be6bc5f5e323c3ff323b
Julianne Costa <julesleighcosta@gmail.com>
2017-08-02 12:41:56 -0700

    checkout works

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -8,38 +8,48 @@ class Cart < ApplicationRecord
   def self.checkout
     in_stock = []
     sold_out = []
-    items = active_cart.products
-    items.each do |item|
-      if item.available_inventory > 0
-        in_stock << item
-        item.available_inventory -= 1
+    cart = Cart.active_cart
+    products = cart.products.to_a
+    products.each do |product|
+      if product.available_inventory > 0
+        in_stock << product
+        product.available_inventory -= 1
       else
-        sold_out << item
+        sold_out << product
       end
     end
-    active_cart.active = false
-    active_cart.save!
-    display_message(sold_out)
-    generate_receipt(in_stock)
+    cart.active = false
+    cart.save!
+    Cart.generate_receipt(in_stock)
+    Cart.display_message(sold_out) if sold_out.length > 0
   end
 
   def self.view_history
-    history = Cart.where(active: false).to_a
+    purchases = []
+    old_carts = Cart.where(active: false).includes(:products).to_a
+    debugger
+    old_carts.each do |cart|
+      cart.products.each do |product|
+        purchases << product
+      end
+    end
+    purchases
   end
 
   def add_product(title)
-    product = Product.where(title: title).to_a
-    Selection.create!(product_id: product.id, cart_id: active_cart.id)
+    product = Product.where(title: title).first
+    Selection.create!(product_id: product.id, cart_id: Cart.active_cart.id)
   end
 
   def remove_product(title)
-    item = Product.where(title: title).to_a
-    record = Selection.where(cart_id: Cart.active_cart.id, product_id: item.id).limit(1).to_a
+    product = Product.where(title: title).first
+    record = Selection.where(cart_id: Cart.active_cart.id, product_id: product.id).limit(1).first
     Selection.destroy(record.id)
   end
 
   def view_cart
-    active_cart.products
+    products = Cart.active_cart.products.to_a
+    products.map { |product| product.title }
   end
 
   private
@@ -49,16 +59,16 @@ class Cart < ApplicationRecord
   end
 
   def self.active_cart
-    Cart.where(active: true).to_a
+    Cart.where(active: true).first
   end
 
-  def display_message(array)
+  def self.display_message(array)
     x = array.map { |el| el.title }.join(", ")
-    puts "We're sorry, but we're sold out of #{x}."
+    puts "We're sorry, but we're sold out of #{x}"
   end
 
-  def generate_receipt(array)
-    x = array.map { |el| el.title }
+  def self.generate_receipt(array)
+    x = array.map { |el| el.title }.join(", ")
     puts "Thank you for your order of: #{x}"
   end
 
