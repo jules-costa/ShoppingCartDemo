1818e5ba8ad27da88c3d86e0184f7985fc858f00
Julianne Costa <julesleighcosta@gmail.com>
2017-08-31 13:38:06 -0700

    add skeletons of methods in each new service object

---
+++ app/services/shopping.rb
@@ -0,0 +1,21 @@
+class ShoppingService
+  def initialize(params)
+
+  end
+  
+  def validate_new_cart
+
+  end
+
+  def add_product(product)
+
+  end
+
+  def remove_product(product)
+
+  end
+
+  def checkout
+
+  end
+end
---
+++ app/services/viewing.rb
@@ -0,0 +1,27 @@
+class ViewingService
+
+  def initialize
+
+  end
+
+  def view_inventory
+
+  end
+
+  def view_cart
+
+  end
+
+  def view_history
+
+  end
+
+  def print_receipt
+
+  end
+
+  def print_message
+
+  end
+
+end
